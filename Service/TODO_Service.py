4952634b2c6a12eb00ce4150e3a886cd719eb261
Oleg-1990 <skorodelov90@gmail.com>
2022-10-08 12:35:43 +0300

    test ssh key

--- Service/TODO_Service.py
+++ Service/TODO_Service.py
@@ -3,6 +3,9 @@ from Repository import Repository
 from TODO_item import TODO_item
 from datetime import datetime
 
-
+class todo_service_item:
+    def __init__(self):
+        pass
+    def add_item(self):
 
 
